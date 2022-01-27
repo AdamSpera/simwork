9f56a81227d006fe69fde239073a3f851e84ec71
AdamSpera <adamspera@hotmail.com>
2022-01-27 13:37:20 -0500

    Boiler Commit

---
+++ app.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+var express = require('express');
+var mysql = require('mysql');
+var app = express();
+
+app.use(express.static('public'));
+var server = app.listen(80, '0.0.0.0', function () {
+    console.log('Server is listening on port 80');
+})
+
+var connection = mysql.createConnection({
+    host: process.env.HOST,
+    user: process.env.MYSQLUSERNAME,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE
+});
+
+connection.connect(function (error) {
+    if (!!error) {
+      console.log(error);
+    } else {
+      console.logF('Database Connected');
+    }
+})
